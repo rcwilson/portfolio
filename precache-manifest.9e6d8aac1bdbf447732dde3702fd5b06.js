f0d744a0044c693d38728cc0a7d4752e223e43d3
richard wilson <rcwilson88@gmail.com>
2020-05-11 21:26:38 -0400

    Updates

---
+++ precache-manifest.9e6d8aac1bdbf447732dde3702fd5b06.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0353906125fdc778db3cb011e2465e3e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5b63ce7913a0cbe3ed73",
+    "url": "/portfolio/static/css/main.16814acf.chunk.css"
+  },
+  {
+    "revision": "56ea933ab8929a51f55b",
+    "url": "/portfolio/static/js/2.44409633.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.44409633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b63ce7913a0cbe3ed73",
+    "url": "/portfolio/static/js/main.f8ada2ad.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5daba7dcefe9371854b34892eca1bdff",
+    "url": "/portfolio/static/media/Resume.5daba7dc.png"
+  },
+  {
+    "revision": "b7d45019b79da4da848fc0a046a527ba",
+    "url": "/portfolio/static/media/Resume.b7d45019.pdf"
+  },
+  {
+    "revision": "46cefec6904dfa6fd2e5e9a2fe6774bb",
+    "url": "/portfolio/static/media/background-evening2.46cefec6.png"
+  },
+  {
+    "revision": "9d1ac4e820590f4f8260a53298053ddc",
+    "url": "/portfolio/static/media/background-night1.9d1ac4e8.png"
+  },
+  {
+    "revision": "10b27d33cb1d1b69e9db7384e3a7e9bd",
+    "url": "/portfolio/static/media/background-noon.10b27d33.png"
+  },
+  {
+    "revision": "f28254009ff66c61f1fc0d3387a69693",
+    "url": "/portfolio/static/media/background-sunset.f2825400.png"
+  },
+  {
+    "revision": "85b94cacd9b0ad1f452e630c9ee19de2",
+    "url": "/portfolio/static/media/blackjack-1.85b94cac.JPG"
+  },
+  {
+    "revision": "5b7e38ab0f4ac9dddf4f1795c4ca9be1",
+    "url": "/portfolio/static/media/blackjack-2.5b7e38ab.JPG"
+  },
+  {
+    "revision": "fd9a52f47cccd87ba9d13f81152b8f93",
+    "url": "/portfolio/static/media/bring-it-1.fd9a52f4.JPG"
+  },
+  {
+    "revision": "1d8e340607a302a202fe680a10249bce",
+    "url": "/portfolio/static/media/bring-it-2.1d8e3406.JPG"
+  },
+  {
+    "revision": "543294a34d01038eb3996d591fd7a4fc",
+    "url": "/portfolio/static/media/friendventure-1.543294a3.JPG"
+  },
+  {
+    "revision": "e0998ffc87eb98c29083528e8a9334a3",
+    "url": "/portfolio/static/media/friendventure-2.e0998ffc.JPG"
+  },
+  {
+    "revision": "2ac519f2c7711255ed5f6424af715c1c",
+    "url": "/portfolio/static/media/friendventure-3.2ac519f2.JPG"
+  },
+  {
+    "revision": "97ca7fedacc5be7a5e01f40b120bbd27",
+    "url": "/portfolio/static/media/gear-icon.97ca7fed.png"
+  },
+  {
+    "revision": "f40f38a57873098988fc7e149518436d",
+    "url": "/portfolio/static/media/headshot_cropped.f40f38a5.jpg"
+  }
+]);
